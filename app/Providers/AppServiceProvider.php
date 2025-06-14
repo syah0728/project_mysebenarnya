f8a2fdbbe01b219e1ef0b25bb9f8f92672cf86f6
KhusnulKotimam <CB23072@adab.umpsa.edu.my>
2025-06-14 23:42:44 +0800

    project

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,20 +3,15 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // ✅ add this line
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot()
     {
         if ($this->app->environment('production')) {
